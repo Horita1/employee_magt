049d8e3fc348c0102c131630aa0b4c26db830f01
shintaro.horita <pp.ppt99@gmail.com>
2023-03-15 17:57:49 +0900

    修正

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,7 +3,13 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.dto.UserSearchRequest;
+import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
 /**
@@ -25,10 +31,22 @@ public class UserController {
 	 * return ユーザー情報一覧画面
 	 */
 	@GetMapping(value = "/user/search")
-	public String display Search(Model model) {
-		model.addAttribute("userSerchRequest",new UserSerchRequest());
+	public String displaySearch(Model model) {
+		model.addAttribute("userSearchRequest",new UserSearchRequest());
 		return "user/serch";
 	}
 	
+	/**
+	 * ユーザー情報検索
+	 * @param userSerchRequest リクエストデータ
+	 * @param model Model
+	 * @param ユーザー情報一覧画面
+	 */
+	@RequestMapping(value = "/user/id_serch",method = RequestMethod.POST)
+	public String search(@ModelAttribute UserSearchRequest userSearchRequest, Model model) {
+		User user = userService.search(userSearchRequest);
+		model.addAttribute("userinfo",user);
+		return "user/search";
+	}
 	
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,5 +1,21 @@
 package com.example.demo.entity;
 
+import java.util.Date;
+
+import lombok.Data;
+
+/**
+ * ユーザー情報　Entity
+ */
+@Data
 public class User {
+	
+	private Long id;
+	private String name;
+	private String address;
+	private String phone;
+	private Date updateDate;
+	private Date createDate;
+	private Date deleteDate;
 
 }
